ecdff0c0413da0cf549b8fbf9c46b22d504c6657
YukiKuroshima <yukikuro1993@gmail.com>
2017-12-17 21:05:29 -0800

    :tada: Mysql working

--- project/__init__.py
+++ project/__init__.py
@@ -2,7 +2,6 @@
 
 
 import os
-import datetime
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
@@ -19,26 +18,31 @@ db = SQLAlchemy(app)
 
 
 # model
-"""
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=False, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
 
-    def __init__(self, username, email):
+    def __init__(self, username):
         self.username = username
-        self.email = email
-        self.created_at = datetime.datetime.utcnow()
-"""
+
+    def tojson(self):
+        return {
+            # 'id': self.id,
+            'username': self.username
+        }
 
 
 # routes
 @app.route('/ping', methods=['GET'])
 def ping_pong():
+    db.session.add(User('TestUser'))
+    db.session.commit()
+
+    user = User.query.filter_by(username='TestUser').first()
+
     return jsonify({
         'status': 'success',
-        'message': 'pong!'
+        'message': 'pong!',
+        'users': user.username
     })
